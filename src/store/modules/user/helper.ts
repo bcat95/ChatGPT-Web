6b915159ecefcf3d43ac0a060644254b8e9f0c9d
bcat95 <bcat9505@gmail.com>
2023-03-17 09:18:47 +0700

    defaultSetting avatar, name, desc

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://chatgptvietnam.org/assets/favicon.png',
+      name: 'chatgptvietnam.org',
+      description: 'Trải nghiệm ngay <a href="https://chatgptvietnam.org" class="text-blue-500" target="_blank" >ChatGPT Vietnamese không cần tài khoản</a>',
     },
   }
 }

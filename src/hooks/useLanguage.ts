cc1560c514fe329ceb4ca8c663b16ef35c3272da
bcat95 <bcat9505@gmail.com>
2023-04-11 08:38:07 +0700

    fix: default return in useLanguange()

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -21,7 +21,7 @@ export function useLanguage() {
         setLocale('zh-TW')
         return zhTW
       default:
-        setLocale('zh-CN')
+        setLocale('en-US')
         return enUS
     }
   })

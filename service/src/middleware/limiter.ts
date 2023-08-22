2af0180cdbaf0e84da18eeaa490c1ecb107469b7
bcat95 <bcat9505@gmail.com>
2023-08-22 13:47:27 +0700

    Update limiter.ts

--- service/src/middleware/limiter.ts
+++ service/src/middleware/limiter.ts
@@ -12,7 +12,7 @@ const limiter = rateLimit({
   max: maxCount,
   statusCode: 200, // 200 means success，but the message is 'Too many request from this IP in 1 hour'
   message: async (req, res) => {
-    res.send({ status: 'Fail', message: 'Too many request from this IP in 1 hour', data: null })
+    res.send({ status: 'Fail', message: 'Giới hạn trò chuyện trong 1h, bạn hãy sử dụng https://mentor.chatvn.org để chat không giới hạn.', data: null })
   },
 })
 
